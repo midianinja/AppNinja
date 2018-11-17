5e3cc762b519e31db20061260aef13f01d24a819
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2018-11-17 16:23:49 -0200

    add basic login flow

--- frontend/screens/SignInScreen.js
+++ frontend/screens/SignInScreen.js
@@ -25,6 +25,7 @@ export default class SignInScreen extends React.Component {
     this.state = {
       email: '',
       password: '',
+      isLoading: false,
     };
   }
 
@@ -35,10 +36,10 @@ export default class SignInScreen extends React.Component {
         <View style={[theme.fullContainer, {flex: 6, paddingVertical: 20}]}>
           <Input placeholder='Usuário' leftIcon={
             <Icon name='user' color='white' />
-          } />
+          } onChangeText={(email) => this.setState({email})} />
           <Input containerStyle={{marginTop: 5}} secureTextEntry placeholder='Senha' leftIcon={
             <Icon name='lock' color='white' />
-          } />
+          } onChangeText={(password) => this.setState({password})} />
           <Button title='LOGIN' onPress={this._signInAsync} />
           <Button {...theme.passwordRecovery} title='Esqueci a senha' onPress={this._passwordResetScreen} />
         </View>
@@ -54,13 +55,41 @@ export default class SignInScreen extends React.Component {
           <Text>Não tem uma conta?</Text>
           <Button title='CRIAR UMA CONTA' {...theme.buttonSecondary} onPress={this._signUpScreen} />
         </View>
+        <Overlay isVisible={this.state.isLoading}>
+          <ActivityIndicator size='large' color='white'/>
+        </Overlay>
       </View>
     );
   }
 
   _signInAsync = async () => {
-    await AsyncStorage.setItem('userToken', 'abc');
-    this.props.navigation.navigate('App');
+    this.setState({isLoading: true});
+
+    fetch(process.env.API_URL + 'auth/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+      }),
+    }).then((response) => {
+      this.setState({isLoading: false});
+      let data = response.json();
+      await AsyncStorage.setItem('userToken', data.token);
+      this.props.navigation.navigate('App');
+    }).catch((error) => {
+      this.setState({isLoading: false});
+      let data = error.json();
+      this.setState({ isLoading: false });
+      showMessage({
+        message: 'Erro',
+        description: data.message,
+        type: 'danger',
+      });
+    });
   };
 
   _passwordResetScreen = () => {
