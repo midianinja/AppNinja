dd0ffcfd93a5875426a3e4634782a30149561ee7
Leonardo A. de Jesus <leoaj89@gmail.com>
2018-11-17 16:15:04 -0200

    Acrescentados parˆ

---
+++ backend/TesteAuth.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+auth_data = {'email': 'teste@teste.com', 'password': 'teste123'}
+response = requests.post('http://127.0.0.1:8000/api/auth/login/', data=auth_data)
+
+if response.status_code == 200:
+    print("Login realizado com sucesso")
+
+    response_json = json.loads(response.content)
+    token = response_json['data']['token']
+    headers = {'Authorization': 'Token ' + token}
+
+    print("Testando logout... ", end='')
+    response_logout = requests.delete('http://127.0.0.1:8000/api/auth/logout/', headers=headers)
+    if response_logout.status_code == 204:
+        print("Logout realizado com sucesso")
+else:
+    print("Erro no login")
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,17 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
