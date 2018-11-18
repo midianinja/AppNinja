9c86b5069e3ee33a9f2c8c64eb99a1b578f657e6
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2018-11-18 05:29:28 -0200

    add user account screen closes #29

--- frontend/navigation/MainTabNavigator.js
+++ frontend/navigation/MainTabNavigator.js
@@ -4,7 +4,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import AccountScreen from '../screens/AccountScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const HomeStack = createStackNavigator({
@@ -25,16 +25,16 @@ HomeStack.navigationOptions = {
   ),
 };
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+const AccountStack = createStackNavigator({
+  Account: AccountScreen,
 });
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+AccountStack.navigationOptions = {
+  tabBarLabel: 'Perfil Ninja',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+      name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'}
     />
   ),
 };
@@ -55,6 +55,6 @@ SettingsStack.navigationOptions = {
 
 export default createBottomTabNavigator({
   HomeStack,
-  LinksStack,
+  AccountStack,
   SettingsStack,
 });
