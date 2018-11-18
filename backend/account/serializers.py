b50e17734dc1e6573e1896276def4cc26dba2458
Luis Fagundes <lhfagundes@gmail.com>
2018-11-18 14:29:21 -0200

    serializer do ninja

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
-from .models import User
+from .models import User, Ninja
+
+class NinjaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ninja
+        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     def create(self, data):
@@ -9,7 +14,11 @@ class UserSerializer(serializers.ModelSerializer):
             data['is_active'] = False
         return super().create(data)
 
+    ninja = NinjaSerializer()
+    
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'password', 'is_superuser', 'groups')
+        fields = ('id', 'email', 'first_name', 'last_name', 'password', 'is_superuser', 'groups', 'ninja')
         extra_kwargs = {'password': {'write_only': True, 'required': False}}
+
+
